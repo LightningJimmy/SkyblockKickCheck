c917ac702b8218b963c7f5f08e4d67d21f21824d
Jimmy <jimmydfang@gmail.com>
2020-05-04 21:30:21 -0700

    creates copy instead of deleting log

--- SkyblockKickCheck.bat
+++ SkyblockKickCheck.bat
@@ -1,23 +1,23 @@
-@echo off
+@echo on
+type "%appdata%\.minecraft\logs\blclient\minecraft\latest.log" > "%appdata%\.minecraft\logs\blclient\minecraft\latest.copy.log"
+
 :afkcheck
+type "%appdata%\.minecraft\logs\blclient\minecraft\latest.log" | findstr /lvg:"%appdata%\.minecraft\logs\blclient\minecraft\latest.copy.log" > "%appdata%\.minecraft\logs\blclient\minecraft\compare.log"
+
 echo Last time checked:
 time /T
->nul find "[Important] This server will restart soon: Scheduled Reboot" "%appdata%\.minecraft\logs\blclient\minecraft\latest.log" && (
-  start "" /wait cmd /c "echo Server Reboot! Time to reconnect!&echo(&pause"
-  type "%appdata%\.minecraft\logs\blclient\minecraft\latest.log" >> "%appdata%\.minecraft\logs\blclient\minecraft\latest.copy.log"
+>nul find "[Important] This server will restart soon: Scheduled Reboot" "%appdata%\.minecraft\logs\blclient\minecraft\compare.log" && (
+  start "" /wait cmd /c "time /T&echo Server Reboot! Time to reconnect!&echo(&pause"
+  type "%appdata%\.minecraft\logs\blclient\minecraft\latest.log" > "%appdata%\.minecraft\logs\blclient\minecraft\latest.copy.log"
   echo Log copied to latest.copy.log
-  del "%appdata%\.minecraft\logs\blclient\minecraft\latest.log"
-  echo latest.log deleted
 ) || (
   echo No server reboot
 )
 
->nul find "You are being transferred to the " "%appdata%\.minecraft\logs\blclient\minecraft\latest.log" && (
-  start "" /wait cmd /c "echo AFK kicked! Time to reconnect!&echo(&pause"
-  type "%appdata%\.minecraft\logs\blclient\minecraft\latest.log" >> "%appdata%\.minecraft\logs\blclient\minecraft\latest.copy.log"
+>nul find "You are being transferred to the " "%appdata%\.minecraft\logs\blclient\minecraft\compare.log" && (
+  start "" /wait cmd /c "time /T&echo AFK kicked! Time to reconnect!&echo(&pause"
+  type "%appdata%\.minecraft\logs\blclient\minecraft\latest.log" > "%appdata%\.minecraft\logs\blclient\minecraft\latest.copy.log"
   echo Log copied to latest.copy.log
-  del "%appdata%\.minecraft\logs\blclient\minecraft\latest.log"
-  echo latest.log deleted
 ) || (
   echo Not AFK
 )
